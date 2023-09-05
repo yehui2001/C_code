6b8a1fde11b2c1302077dd5516bf66965a09d2d4
叶珲 <365526026@qq.com>
2023-09-05 21:01:33 +0800

    2023.9.5
    1111

--- 王道课后习题/Chapte 2/2.2.11.cpp
+++ 王道课后习题/Chapte 2/2.2.11.cpp
@@ -26,18 +26,36 @@ void Print_List(Seqlist &L){
 }
 
 void Find_midpos(Seqlist &L){
-    
+    int step = 0,pos = 0;
+    for (int i = 0,j = L.length/2; i < L.length/2 && j < L.length;){
+        if(L.data[i] < L.data[j]){
+            step++;                     //
+            if(step==L.length/2){
+                pos = i;
+                break;
+            }
+            i++;
+        }
+        else{
+            step++;
+            if(step==L.length/2){
+                pos = j;
+                break;
+            }
+            j++;
+        }
+    }
+    cout <<"中位数:"<< L.data[pos] <<endl;
 }
 
 int main(){
     Seqlist L;
     Init_List(L);
-    cout<<"请输入要插入的元素个数:"<<endl;
+    cout<<"请输入要插入的元素个数(偶数):"<<endl;
     cin >>L.length;
     for(int i = 0; i < L.length; i++){
         cin >> L.data[i];
     }
     Print_List(L);
-    Delete_scope(L,2,8);
-    Print_List(L);
+    Find_midpos(L);
 }
