d7c4a2ef483ca141702324d3953088ebe67a2e12
叶珲 <365526026@qq.com>
2023-09-17 21:15:22 +0800

    2023.9.17_21:14
    weary

--- 王道课后习题/Chapter 3/2.3.22.cpp
+++ 王道课后习题/Chapter 3/2.3.22.cpp
@@ -1,6 +1,5 @@
 //该程序实现基于带头节点的单链表的定义及基础操作
 #include<iostream>
-#include <cstdlib>          //随机数
 using namespace std;
 
 typedef int ElemType;
@@ -9,7 +8,6 @@ typedef struct LNode{
     ElemType data;          //数据域
     struct LNode *next;     //指针域
 }LNode,*LinkList;           //重命名后的Linklist常用来定义单链表的头指针
-
 
 //初始化单链表
 bool Init_List(LinkList &L){
@@ -38,6 +36,7 @@ LinkList Create_List_tail(LinkList &L){
     return L;
 }
 
+
 //打印单链表
 void Print_Linklist(LinkList &L){
     LNode *p = L->next;                      //从头指针后的第一个结点开始打印
@@ -49,55 +48,23 @@ void Print_Linklist(LinkList &L){
     cout << endl;
 }
 
-//随机数    产生 [a,b] 范围内到随机数 
-int Rand(){
-    int a = 1,b = 8;
-    srand((unsigned)time(NULL));        //添加当前时间为随机数种子，不加这行就产生伪随机数
-    return a + rand()%(b-a+1);
-}
-
-//手动创建让链表带环
-void Generate_circular(LinkList &L){
-    LNode *p = L,*q;
-    int Tag = 0;                                //在第五个结点位置进环
-    while(Tag!=Rand()){                         
-        p = p->next;
-        Tag++;
-    }
-    q = p;
-    p = L;
+//找倒数第k个结点，双指针
+bool Find_LNode(LinkList &L,int k){
+    LNode *p = L,*q=NULL;
+    int Tag = 0;
     while(p->next!=NULL){
         p = p->next;
-    }
-    p->next = q;
-
-}
-
-//先找到相遇点，又因为a = n*r - x,所以从头结点到入口处的距离 与 从相遇点处移动n-1圈后到入口处的距离 相同
-//a:头结点到入口处的距离
-//r:环长
-//x:入口处到相遇点处的距离
-LNode *Find_circular(LinkList &L){
-    LNode *fast = L,*slow = L;
-    while(fast!=NULL&&fast->next!=NULL){        //为什么要加两个判断条件，因为fast指针一次跳两步，有可能会跳在最后一个结点停止，也有可能会跳在NULL指针上停止
-        fast = fast->next->next;
-        slow = slow->next;
-        if(fast==slow)  break;                  //找到相遇点
-    }
-    if(fast==NULL||fast->next==NULL)
-        return NULL;
-    LNode *p1 = L,*p2 = slow;                //分别指向开始点，相遇点
-    while(p1!=p2){
-        p1 = p1->next;
-        p2 = p2->next;
-    }
-    LNode *p = L;
-    int Tag = 0;
-    while(p!=p1){
-        p = p->next;
         Tag++;
+        if(Tag==k){
+            q = L;
+        }
+        if(Tag>=k)
+        q = q->next;
     }
-    cout << "入口处的位置在" << Tag <<"个结点处"<<endl;
+    if(q==NULL)
+    return 0;
+    cout << "倒数第"<< k <<"个元素是:" << q->data << endl;
+    return 1;
 }
 
 int main(){
@@ -105,7 +72,6 @@ int main(){
     Init_List(L);
     Create_List_tail(L);
     Print_Linklist(L);
-    Generate_circular(L);
-    Find_circular(L);
+    Find_LNode(L,4);
     return 0;
 }
